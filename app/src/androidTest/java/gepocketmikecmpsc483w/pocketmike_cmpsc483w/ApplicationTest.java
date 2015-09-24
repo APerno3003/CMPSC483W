57df218257b328e42d9b3cb170f2f9b683f2f247
APerno3003 <APerno3003@gmail.com>
2015-09-24 19:16:00 -0400

    Start

---
+++ app/src/androidTest/java/gepocketmikecmpsc483w/pocketmike_cmpsc483w/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gepocketmikecmpsc483w.pocketmike_cmpsc483w;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
