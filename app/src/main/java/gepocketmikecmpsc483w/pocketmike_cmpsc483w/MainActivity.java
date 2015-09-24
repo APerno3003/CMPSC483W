8f4e34b3b5d4e2bced6bcafa960d42c2baf644f8
APerno3003 <APerno3003@gmail.com>
2015-09-24 19:45:37 -0400

    Main Screen Done

--- app/src/main/java/gepocketmikecmpsc483w/pocketmike_cmpsc483w/MainActivity.java
+++ app/src/main/java/gepocketmikecmpsc483w/pocketmike_cmpsc483w/MainActivity.java
@@ -1,16 +1,36 @@
 package gepocketmikecmpsc483w.pocketmike_cmpsc483w;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button Mode1Button;
+    Button Mode2Button;
+    Button Mode3Button;
+    Button SettingsButton;
+    Button CopyrightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        Mode1Button = (Button)findViewById(R.id.Mode1Button);
+        Mode1Button.setOnClickListener(this);
+        Mode2Button = (Button)findViewById(R.id.Mode2Button);
+        Mode2Button.setOnClickListener(this);
+        Mode3Button = (Button)findViewById(R.id.Mode3Button);
+        Mode3Button.setOnClickListener(this);
+        SettingsButton = (Button)findViewById(R.id.SettingsButton);
+        SettingsButton.setOnClickListener(this);
+        CopyrightButton = (Button)findViewById(R.id.CopyrightButton);
+        CopyrightButton.setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +53,51 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void Mode1ButtonOnClick()
+    {
+        startActivity(new Intent("Mode1Activity"));
+    }
+
+    private void Mode2ButtonOnClick()
+    {
+        startActivity(new Intent("Mode2Activity"));
+    }
+
+    private void Mode3ButtonOnClick()
+    {
+        startActivity(new Intent("Mode3Activity"));
+    }
+
+    private void SettingsButtonOnClick()
+    {
+        startActivity(new Intent("SettingsActivity"));
+    }
+
+    private void CopyrightButtonOnClick()
+    {
+        startActivity(new Intent("CopyrightActivity"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Mode1Button:
+                Mode1ButtonOnClick();
+                break;
+            case R.id.Mode2Button:
+                Mode2ButtonOnClick();
+                break;
+            case R.id.Mode3Button:
+                Mode3ButtonOnClick();
+                break;
+            case R.id.SettingsButton:
+                SettingsButtonOnClick();
+                break;
+            case R.id.CopyrightButton:
+                CopyrightButtonOnClick();
+                break;
+        }
     }
 }
